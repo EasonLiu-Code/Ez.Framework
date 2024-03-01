b892790c416d7c97800a486a8b684e0cbb406b59
刘煜松 <yusong.liu@1hai.cn>
2024-03-01 15:51:42 +0800

    init

---
+++ Ez.Framework.Host/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ez.Framework.Controllers;
+
+/// <summary>
+/// 数据测试
+/// </summary>
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class TestController
+{
+    /// <summary>
+    /// TestXml
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<string> TestXmlAsync()
+    {
+        return "TestXml";
+    }
+}

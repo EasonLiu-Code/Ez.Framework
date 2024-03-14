7e6025c90a1396625eb0035084ded5183c8f45a7
刘煜松 <yusong.liu@1hai.cn>
2024-03-14 14:43:14 +0800

    支持EndPoints

---
+++ Ez.Framework.Host/EndPoints/Tests.cs
@@ -0,0 +1,19 @@
+﻿using Carter;
+
+namespace Ez.Framework.EndPoints;
+
+/// <summary>
+/// Tests-EndPoint
+/// </summary>
+public class Tests:ICarterModule
+{
+    /// <summary>
+    /// AddRoutes
+    /// </summary>
+    /// <param name="app"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("tests", (string request) => Task.FromResult($"测试EndPoints+{ request}")).WithName("testEndPointByCater");
+    }
+}

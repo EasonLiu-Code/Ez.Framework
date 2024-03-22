1f691097ddeae09591a25b7876c37ebcee120db8
刘煜松 <yusong.liu@1hai.cn>
2024-03-22 21:00:03 +0800

    add uow

---
+++ Ez.Framework.Persistence/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace Persistence;
+
+internal sealed class UnitOfWork(ApplicationDbContext _dbContext)
+{
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}

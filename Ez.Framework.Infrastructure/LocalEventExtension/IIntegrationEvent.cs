8445c5dfc558ee0383f782f1506be86ae32be6f9
刘煜松 <yusong.liu@1hai.cn>
2024-04-15 15:48:52 +0800

    clean code

--- Ez.Framework.Infrastructure/LocalEventExtension/IIntegrationEvent.cs
+++ Ez.Framework.Infrastructure/LocalEventExtension/IIntegrationEvent.cs
@@ -8,8 +8,6 @@ public interface IIntegrationEvent:INotification
     /// <summary></summary>
     Guid Id { get; init; }
 
-    /// <summary>
-    ///是否日志记录
-    /// </summary>
-    public bool IsLog { get; set; }
+    /// <summary></summary>
+    public bool IsLog { get; init; }
 }
--- Ez.Framework.Infrastructure/LocalEventExtension/IntegrationEvent.cs
+++ Ez.Framework.Infrastructure/LocalEventExtension/IntegrationEvent.cs
@@ -3,9 +3,4 @@ namespace Ez.Infrastructure.LocalEventExtension;
 /// <summary></summary>
 public abstract record IntegrationEvent(Guid Id,bool IsLog):IIntegrationEvent
 {
-    /// <summary></summary>
-    public Guid Id { get; init; }
-
-    /// <summary></summary>
-    public bool IsLog { get; set; } = false;
 }

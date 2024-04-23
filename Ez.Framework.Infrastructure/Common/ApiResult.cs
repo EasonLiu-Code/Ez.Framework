2c2a8ab5fffe75ff30c5ee26a92a23982f1d121c
刘煜松 <yusong.liu@1hai.cn>
2024-04-23 16:07:41 +0800

    init

--- Ez.Framework.Infrastructure/Common/ApiResult.cs
+++ Ez.Framework.Infrastructure/Common/ApiResult.cs
@@ -3,23 +3,21 @@
 /// <summary>
 /// </summary>
 /// <typeparam name="TResult"></typeparam>
-public sealed class ApiResult<TResult>
+public  class ApiResult<TResult>
 {
+    public ApiResult()
+    {
+      
+    }
+    
+    private string Message { get; set; }
     public ApiResult(int errorCode, string message, TResult result)
     {
         Message = message;
         this.Result = result;
     }
-
-    public ApiResult()
-    {
-      
-    }
     public bool IsSuccess { get; set; }
-
     public int ErrorCode { get; set; }
-
-    public string Message { get; set; }
     public TResult Result { get; set; }
     public override string ToString()
     {
